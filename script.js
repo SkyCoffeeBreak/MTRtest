0ed911daadb93e3d80d9a294bbd76cfad5daa61f
SkyCoffeeBreak <127721701+SkyCoffeeBreak@users.noreply.github.com>
2024-07-24 09:43:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("modal-img");
+var captionText = document.getElementById("caption");
+var galleryItems = document.querySelectorAll(".gallery-item");
+
+galleryItems.forEach(function(item) {
+    item.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal image, close the modal
+modal.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
