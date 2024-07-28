9a1943479ac80a5daf9305319720cfc0ef0cbef6
SkyCoffeeBreak <127721701+SkyCoffeeBreak@users.noreply.github.com>
2024-07-28 09:22:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-// Get the modal
-var modal = document.getElementById("modal");
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modal-img");
+    var captionText = document.getElementById("caption");
+    var closeBtn = document.getElementsByClassName("close")[0];
+    var prevBtn = document.getElementsByClassName("prev")[0];
+    var nextBtn = document.getElementsByClassName("next")[0];
+    var currentIndex;
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var modalImg = document.getElementById("modal-img");
-var captionText = document.getElementById("caption");
-var galleryItems = document.querySelectorAll(".gallery-item");
+    var images = document.querySelectorAll(".gallery-item");
+    images.forEach(function (img, index) {
+        img.onclick = function () {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+            currentIndex = index;
+        };
+    });
 
-galleryItems.forEach(function(item) {
-    item.onclick = function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
-});
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal image, close the modal
-modal.onclick = function(event) {
-    if (event.target === modal) {
+    closeBtn.onclick = function () {
         modal.style.display = "none";
-    }
-}
+    };
+
+    prevBtn.onclick = function () {
+        currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+        modalImg.src = images[currentIndex].src;
+        captionText.innerHTML = images[currentIndex].alt;
+    };
+
+    nextBtn.onclick = function () {
+        currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+        modalImg.src = images[currentIndex].src;
+        captionText.innerHTML = images[currentIndex].alt;
+    };
+
+    modal.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+});
